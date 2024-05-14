b127dae69511e42933bdb38369d245a862c42093
rodrigo <rodrigo201902404025@gmail.com>
2024-05-14 18:07:08 -0300

    Initial commit

---
+++ src/main/java/soft/rodi38/eventorganizer/EventorganizerApplication.java
@@ -0,0 +1,13 @@
+package soft.rodi38.eventorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventorganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventorganizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/soft/rodi38/eventorganizer/EventorganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package soft.rodi38.eventorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventorganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
