084d45809f3c78cbc4de0efb515347fc7789942b
rodi38 <rodrigorochaed1@gmail.com>
2024-05-28 17:32:47 -0300

    attendee create test

--- src/main/java/soft/rodi38/eventorganizer/controller/AttendeeController.java
+++ src/main/java/soft/rodi38/eventorganizer/controller/AttendeeController.java
@@ -4,10 +4,9 @@ package soft.rodi38.eventorganizer.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import soft.rodi38.eventorganizer.model.dto.AttendeeRecord;
+import soft.rodi38.eventorganizer.model.dto.request.CreateAttendeeRequest;
 import soft.rodi38.eventorganizer.service.AttendeeService;
 
 import java.util.List;
@@ -25,5 +24,11 @@ public class AttendeeController {
         List<AttendeeRecord> response = attendeeService.findAll();
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<AttendeeRecord> create(@RequestBody CreateAttendeeRequest request) {
+        AttendeeRecord response = attendeeService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- src/test/java/soft/rodi38/eventorganizer/controller/AttendeeControllerTest.java
+++ src/test/java/soft/rodi38/eventorganizer/controller/AttendeeControllerTest.java
@@ -1,18 +1,26 @@
 package soft.rodi38.eventorganizer.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import soft.rodi38.eventorganizer.model.dto.AttendeeRecord;
+import soft.rodi38.eventorganizer.model.dto.request.CreateAttendeeRequest;
+import soft.rodi38.eventorganizer.model.entity.Attendee;
 import soft.rodi38.eventorganizer.service.AttendeeService;
+
+import java.util.UUID;
 
 @WebMvcTest(AttendeeController.class)
 @AutoConfigureMockMvc
@@ -26,6 +34,28 @@ public class AttendeeControllerTest {
     @MockBean
     private AttendeeService attendeeService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Attendee attendee;
+    private AttendeeRecord attendeeRecord;
+    private CreateAttendeeRequest createAttendeeRequest;
+    @BeforeEach
+    void setUp() {
+        createAttendeeRequest = new CreateAttendeeRequest("Beatrice", "beato2battler@email.com");
+        UUID attendeeUUID = UUID.randomUUID();
+
+        attendee = new Attendee();
+        attendee.setId(attendeeUUID);
+        attendee.setName(createAttendeeRequest.name());
+        attendee.setEmail(createAttendeeRequest.email());
+
+        attendeeRecord = new AttendeeRecord(attendeeUUID, attendee.getName(), attendee.getEmail(), null);
+
+
+
+    }
+
 
     @Test
     @WithMockUser
@@ -35,6 +65,22 @@ public class AttendeeControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
     }
+
+    @Test
+    @WithMockUser
+    void shouldCreateAttendee() throws Exception {
+        Mockito.when(attendeeService.create(Mockito.any(CreateAttendeeRequest.class))).thenReturn(attendeeRecord);
+        mockMvc.perform(MockMvcRequestBuilders.post("/attendees")
+               .with(SecurityMockMvcRequestPostProcessors.csrf())
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(objectMapper.writeValueAsString(createAttendeeRequest)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(attendee.getId().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(attendee.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(attendee.getEmail()));
+
+    }
+
 
 
 
